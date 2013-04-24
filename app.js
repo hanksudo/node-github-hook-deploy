3bc80aa84b3fa40e9de44625cf76d5079661a57c
Hank Wang <drapho@gmail.com>
2013-04-25 01:36:24 +0800

    autodeploy
    Signed-off-by: Hank Wang <drapho@gmail.com>

---
+++ app.js
@@ -0,0 +1,21 @@
+var githubhook = require('githubhook'),
+    servers = {
+        'hook': 'https://github.com/hankwang/github-hook-deploy'
+    };
+
+
+var thishook = githubhook(3131, servers, function (err, payload) {
+    if (!err) {
+        var deploySh = require('child_process').spawn('sh', [ 'deploy.sh' ], {
+            cwd: process.env.PWD
+        });
+
+        deploySh.stdout.on('data', function(data) {
+            var buff = new Buffer(data);
+            console.log(buff.toString('utf8'));
+        });
+        // console.log(payload); // payload is the JSON blob that github POSTs to the server
+    } else {
+        console.log(err);
+    }
+});
